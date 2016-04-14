57586a2c803c5c2f97e4f3ef84add168594d5192
Rodrigo Recoba <Rodrigo33024@gmail.com>
2016-04-14 00:13:32 -0500

    Adds react components

---
+++ app/assets/javascripts/frontend/components/TweetList.jsx
@@ -0,0 +1,14 @@
+import Tweet from "./Tweet"
+export default class TweetList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul className="collection">
+          <Tweet />
+          <Tweet />
+          <Tweet />
+        </ul>
+      </div>
+    )
+  }
+}
---
+++ app/assets/javascripts/frontend/main.jsx
@@ -0,0 +1,23 @@
+import TweetBox from './components/TweetBox'
+import TweetList from './components/TweetList'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <TweetBox />
+        <TweetList />
+      </div>
+    )
+  }
+}
+
+
+let documentReady = () => {
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
